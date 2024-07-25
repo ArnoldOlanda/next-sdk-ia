44fa28f50b1244d61baaf435777049869f2c8317
Arnold Olanda <olanda188@gmail.com>
2024-07-24 23:20:40 -0500

    initial commit

---
+++ app/actions.tsx
@@ -0,0 +1,16 @@
+'use server';
+
+import { createStreamableValue } from 'ai/rsc';
+import { CoreMessage, streamText } from 'ai';
+import { google } from '@ai-sdk/google';
+
+export async function continueConversation(messages: CoreMessage[]) {
+    console.log(messages)
+    const result = await streamText({
+        model: google('models/gemini-1.5-flash'),
+        messages,
+    });
+
+    const stream = createStreamableValue(result.textStream);
+    return stream.value;
+}
